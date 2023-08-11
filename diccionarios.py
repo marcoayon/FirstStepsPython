6a3311ca0c5227326341fd73508f5208d8bc9052
marcoayon <64885822+marcoayon@users.noreply.github.com>
2023-08-11 01:57:22 -0500

    Update diccionarios.py

--- diccionarios.py
+++ diccionarios.py
@@ -5,6 +5,13 @@ persona={
     'materno':'Ayon',
     'edad':49
 }
+
+#El método get se usa para acceder al valor de una clave    
+
+print(persona.get('nombre'))
+
+#otra forma de acceder a los valores es usando los corchetes, esta forma es la mas usada por que usa menos código que get
+
 print(f'Su nombre es {persona["nombre"]} su apellido paterno es {persona["paterno"]} y su apellido materno es {persona["materno"]}.'
       f' Esta persona tiene la edad de {persona["edad"]} años')
 
@@ -71,3 +78,43 @@ else:
 for nombre in lista.keys():
     if nombre == 'Eduardo':
         print(f'Ahora la edad de {nombre} es de {lista["Eduardo"]}')
+
+"""
+Ejercicio en donde debo crear una lista de alumnos con sus respectivas notas para luego listar la cantidad de alumnos 
+y luego debo saber el promedio simple de los alumnos
+"""
+
+print('=======================================================================================')
+print('=======================================================================================')
+
+listaalumnos = {
+    'Sandra' : 14.55,
+    'Mafer' : 16.24,
+    'Fabiana' : 15.64,
+    'Marco' : 14.87,
+    'Rosa' : 10.25,
+    'Jesus' : 12.64,
+    'Renato' : 14.72,
+    'Sueli' : 15.31,
+    'Gino' : 14.17,
+    'Gino Paolo' : 16.72
+}
+suma_nota = 0
+
+
+for alumno in listaalumnos.keys():
+    print(f'La nota del alumno(a) {alumno} es {listaalumnos[alumno]}')
+    suma_nota = suma_nota + listaalumnos[alumno]
+
+print(f'El promedio del aula es {suma_nota / len(listaalumnos.keys())}')
+
+#Otra forma de calcular el promedio del salon es:
+suma_nota = 0
+notas = listaalumnos.values() #Con este método obtengo los valores asignados a cada key y se crea una lista
+for nota in notas:
+    suma_nota = suma_nota + nota
+
+total_alumno = len(listaalumnos.keys())
+
+print(f'El promedio del salón es {suma_nota / total_alumno}')
+
