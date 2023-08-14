62002eddbfcca7f5cc5dd1d8f924015352c23293
marcoayon <64885822+marcoayon@users.noreply.github.com>
2023-08-14 18:36:16 -0500

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -25,3 +25,50 @@ def datos_diccionario(nombre,apellidopaterno,apellidomaterno,edad):
     print (datos_alumno)
 
 datos_diccionario('Marco', 'Diaz', 'Ayon',49)
+
+
+#Función con argumentos predeterminados
+print('========FUNCIÓN CON ARGUMENTOS PREDETERMINADOS===================')
+from datetime import timedelta,datetime
+
+def calcula_horas(tiempo = 51):
+    ahora = datetime.now()
+    aterriza = ahora + timedelta(hours = tiempo)
+    return aterriza.strftime("Aterriza: %A %H:%M")
+
+print (calcula_horas())
+
+print('========FUNCIÓN CON ARGUMENTOS SIN PREDETERMINAR Y ARGUMENTOS PREDETERMINADOS===================')
+
+def calcula_aterrizaje(planeta, tiempo=51):
+    ahora = datetime.now()
+    aterriza = ahora + timedelta(hours=tiempo)
+    return aterriza.strftime(f"Llegará a {planeta} el día : %A a las %H:%M")
+    
+print(calcula_aterrizaje('Luna'))
+
+print('========EJERCICIO CON ARGUEMNTO DE VARIABLE===================')
+
+#Un argumento variable permite recibir una cantidad de variables sin haberlos definido.
+#para ello se debe anteponer un asterísco (*)
+
+def argumento_variable(*variables):
+    tiempo = sum(variables)
+    if tiempo < 60:
+        return f'El tiempo que falta es de {tiempo} minutos'
+    else:
+        return f'El tiempo que falta es de {tiempo/60} horas'
+
+print(argumento_variable(15,10,6))
+print(argumento_variable(15,60,6))
+
+print('========EJERCICIO CON ARGUEMNTO DE PALABRA CLAVE VARIABLE===================')
+#Para que un argumento acepte palabra clave variable debe usarse doble asterísco (**)
+
+def argumento_palabraclave_variable(**argumento):
+    print(f'La cantidad de tripulantes en el avión es de {len(argumento)} y está conformada por:')
+    for titulo, nombre in argumento.items():
+        print (f'{titulo}: {nombre}')
+
+argumento_palabraclave_variable(Piloto='Marco',Flyhoster='Ariana')
+
